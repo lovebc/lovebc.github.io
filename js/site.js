2c8e34da95b57e6561de1cf1d2cbe3dd0f8d6338
Olivia Guest <guest.olivia@gmail.com>
2018-04-04 18:20:12 +0100

    better scrolling to anchors

--- js/site.js
+++ js/site.js
@@ -2,42 +2,28 @@ $(document).ready(function() {
 
   // Variables
   var $codeSnippets = $('.code-example-body'),
-      $nav = $('.navbar'),
-      $body = $('body'),
-      $window = $(window),
-      $popoverLink = $('[data-popover]'),
-      navOffsetTop = $nav.offset().top,
-      $document = $(document),
-      entityMap = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        '"': '&quot;',
-        "'": '&#39;',
-        "/": '&#x2F;'
-      }
+    $nav = $('.navbar'),
+    $body = $('body'),
+    $window = $(window),
+    $popoverLink = $('[data-popover]'),
+    navOffsetTop = $nav.offset().top,
+    $document = $(document),
+    entityMap = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': '&quot;',
+      "'": '&#39;',
+      "/": '&#x2F;'
+    }
 
   function init() {
     $window.on('scroll', onScroll)
     $window.on('resize load', resize)
     $popoverLink.on('click', openPopover)
     $document.on('click', closePopover)
-    $('a[href^="#"]').on('click', smoothScroll)
-    buildSnippets();
-  }
 
-  function smoothScroll(e) {
-    e.preventDefault();
-    $(document).off("scroll");
-    var target = this.hash,
-        menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top-40
-    }, 0, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-    });
+
   }
 
   function openPopover(e) {
@@ -45,21 +31,15 @@ $(document).ready(function() {
     closePopover();
     var popover = $($(this).data('popover'));
     popover.toggleClass('open');
-    popover.css('left', "-"+String(Math.abs(popover.width()-popover.parent().width())/2)+"px");
+    popover.css('left', "-" + String(Math.abs(popover.width() - popover.parent().width()) / 2) + "px");
     e.stopImmediatePropagation();
   }
 
   function closePopover(e) {
-    if($('.popover.open').length > 0) {
+    if ($('.popover.open').length > 0) {
       $('.popover').removeClass('open')
     }
   }
-
-  $("#button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#elementtoScrollToID").offset().top
-    }, 2000);
-});
 
   function resize() {
     $body.removeClass('has-docked-nav')
@@ -68,28 +48,26 @@ $(document).ready(function() {
   }
 
   function onScroll() {
-    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
+    if (navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
       $body.addClass('has-docked-nav')
+
     }
-    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
+    if (navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
       $body.removeClass('has-docked-nav')
     }
   }
 
-  function escapeHtml(string) {
-    return String(string).replace(/[&<>"'\/]/g, function (s) {
-      return entityMap[s];
-    });
-  }
-
-  function buildSnippets() {
-    $codeSnippets.each(function() {
-      var newContent = escapeHtml($(this).html())
-      $(this).html(newContent)
-    })
-  }
-
-
   init();
+
+});
+
+
+$(document).on('click', 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+  window.location.hash = $.attr(this, 'href')
 
 });
