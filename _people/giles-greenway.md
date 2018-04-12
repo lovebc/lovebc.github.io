e0a027ffe4ea71067465c180a2033e939f20dc38
Olivia Guest <guest.olivia@gmail.com>
2018-04-12 12:28:25 +0100

    really improved

---
+++ _people/giles-greenway.md
@@ -0,0 +1,20 @@
+---
+firstname: Giles
+surname: Greenway
+role: research-software-engineer
+order: 2
+
+email: g.greenway@ucl.ac.uk
+
+website:
+twitter:
+researchgate:
+impactstory:
+orcid:
+github:
+
+image_path: /images/people/giles.jpg
+
+bio:
+
+---
--- js/site.js
+++ js/site.js
@@ -62,12 +62,12 @@ $(document).ready(function() {
 });
 
 
-$(document).on('click', 'a[href^="#"]', function(event) {
-  event.preventDefault();
-
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 500);
-  window.location.hash = $.attr(this, 'href')
-
-});
+// $(document).on('click', 'a[href^="#"]', function(event) {
+//   event.preventDefault();
+//
+//   $('html, body').animate({
+//     scrollTop: $($.attr(this, 'href')).offset().top
+//   }, 500);
+//   window.location.hash = $.attr(this, 'href')
+//
+// });
