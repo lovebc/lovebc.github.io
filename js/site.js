649b67cb856aa39ab1b87237fd2386f3207428b8
Bradley Love <bradley.c.love@gmail.com>
2026-04-28 10:28:00 -0600

    cleaning up unused files

--- js/site.js
+++ js/site.js
@@ -60,14 +60,3 @@ $(document).ready(function() {
   init();
 
 });
-
-
-// $(document).on('click', 'a[href^="#"]', function(event) {
-//   event.preventDefault();
-//
-//   $('html, body').animate({
-//     scrollTop: $($.attr(this, 'href')).offset().top
-//   }, 500);
-//   window.location.hash = $.attr(this, 'href')
-//
-// });
